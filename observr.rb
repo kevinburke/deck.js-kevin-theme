c19fad9ca83cd9786cda7ac8ed72d9ce47cedd2e
Kevin Burke <kev@inburke.com>
2017-10-05 10:25:41 -0400

    Updates for "Becoming Go Contributor" talk

--- observr.rb
+++ observr.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env observr
 
-watch( 'scss/(.*)\.scss' )  {|md| cmd = "sass --style compressed #{md[0]} > css/#{md[1]}.css"; puts cmd; system(cmd) }
-
+dirname = File.dirname(__FILE__)
+watch( 'scss/(.*)\.scss' )  {|md| cmd = "sass --style compressed #{File.join(dirname, md[0])} > #{File.join(dirname, "css", md[1] + ".css")}"; puts cmd; system(cmd) }
