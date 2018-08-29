a099af124af2483cd661966bd7623b5f66781b63
Kevin Burke <kev@inburke.com>
2018-08-28 18:49:51 -0600

    CSS tweaks
    New slides are designed for a screen that is 1900x1080. Added several
    helper CSS classes for citations, H2+image slides, plus long H2
    content slides.
    
    Text should read much better now without having to zoom in and out all
    of the time.

--- observr.rb
+++ observr.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env observr
 
 dirname = File.dirname(__FILE__)
-watch( 'scss/(.*)\.scss' )  {|md| cmd = "sass --style compressed #{File.join(dirname, md[0])} > #{File.join(dirname, "css", md[1] + ".css")}"; puts cmd; system(cmd) }
+watch( 'scss/(.*)\.scss' )  {|md| cmd = "sass --style expanded #{File.join(dirname, md[0])} > #{File.join(dirname, "css", md[1] + ".css")}"; puts cmd; system(cmd) }
